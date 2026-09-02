75fbea274085ecf107f0a29254ff2278da735763
Igniy <igniy159@gmail.com>
2026-09-03 00:16:31 +0400

    part 7. add alembic

--- learning_info/filter_select.py
+++ learning_info/filter_select.py
@@ -1,6 +1,6 @@
 from session_first_model import session, Branch
 from sqlalchemy import select
-from select_orm import add_branch, del_branches
+from learning_info.select_orm import add_branch, del_branches
 
 
 def orm_operation():
--- learning_info/state_orm_obj.py
+++ learning_info/state_orm_obj.py
@@ -1,4 +1,4 @@
-from session_first_model import session, Branch
+from learning_info.session_first_model import session, Branch
 from sqlalchemy import inspect, select, text
 
 def check_branches():
